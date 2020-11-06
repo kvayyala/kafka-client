4c5a4f19edf7711276af394cb610f47078d39390
Kumar Vayyala <kumar.v18@gmail.com>
2020-11-05 23:38:30 -0800

    kafka ans spring version changes

--- src/main/java/com/first/learn/es/KafkautilsApplication.java
+++ src/main/java/com/first/learn/es/KafkautilsApplication.java
@@ -5,11 +5,13 @@ import java.util.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.first.learn.ist.es.data.events.APPlicationProperties;
 import com.first.learn.ist.es.data.events.ESDataKafkaConsumer;
 
 @SpringBootApplication
+@ComponentScan("com.first.learn")
 public class KafkautilsApplication {
 
 	final static Properties props = new Properties();
@@ -20,10 +22,10 @@ public class KafkautilsApplication {
 		props.put("key.deserializer", appProperties.getKeyDeserializer());
 		props.put("value.deserializer", appProperties.getValueDeserializer());
 		props.put("group.id", appProperties.getGroupId());
-		props.put("ssl.keystore.location", appProperties.getSslKeystoreLocation());
-		props.put("ssl.keystore.password", appProperties.getSslKeystorePassword());
-		props.put("ssl.truststore.location", appProperties.getSslTruststoreLocation());
-		props.put("ssl.truststore.password", appProperties.getSslTruststorePassword());
+		//props.put("ssl.keystore.location", appProperties.getSslKeystoreLocation());
+		//props.put("ssl.keystore.password", appProperties.getSslKeystorePassword());
+		//props.put("ssl.truststore.location", appProperties.getSslTruststoreLocation());
+		//props.put("ssl.truststore.password", appProperties.getSslTruststorePassword());
 		props.put("security.protocol", appProperties.getSecurityProtocol());
 		props.put("sasl.mechanism", appProperties.getSaslMechanism());
 		props.put("auto.offset.reset", appProperties.getAutoOffsetReset());
@@ -34,6 +36,5 @@ public class KafkautilsApplication {
 	public static void main(final String[] args) {
 		SpringApplication.run(KafkautilsApplication.class, args);
 		ESDataKafkaConsumer.consume(props);
-		//ConsumersCheck.consume(props);
 	}
 }
